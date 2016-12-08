d4322379b220e0417e47c6bf9c2e88948b3e880d
zhan1182 <jz2776@columbia.edu>
2016-12-07 21:44:48 -0500

    group all reviews for restaurant

--- preprocessing/get_data_by_key.py
+++ preprocessing/get_data_by_key.py
@@ -2,51 +2,46 @@
 
 import json
 
-raw_business_file = 'yelp_academic_dataset_business.json'
-raw_user_file = 'yelp_academic_dataset_user.json'
-raw_review_file = 'yelp_academic_dataset_review.json'
-raw_tip_file = 'yelp_academic_dataset_tip.json'
-raw_checkin_file = 'yelp_academic_dataset_checkin.json'
+data_dir = './../data/'
 
-def get_json(file_name):
-    with open(file_name, 'r') as json_file:
-        lines = json_file.readlines()
-
-    return [json.loads(s) for s in lines]
+raw_business_file = data_dir + 'yelp_academic_dataset_business.json'
+raw_review_file = data_dir + 'yelp_academic_dataset_review.json'
 
 def get_restaruants():
-    business_json_list = get_json(raw_business_file)
-
-    business_category_set = set([])
     restaurant_list = []
-    for business_json in business_json_list:
-        business_category = business_json['categories']
 
-        if 'Restaurants' in business_category:
-            for category in business_category:
-                business_category_set.add(category)
-            restaurant_list.append(business_json['business_id'])
-            # print(business_category)
+    with open(raw_business_file, 'r') as f:
+        while True:
+            line = f.readline()
 
-    print(len(business_category_set))
-    print(len(business_json_list))
+            if not line:
+                break
+
+            business_json = json.loads(line)
+            business_category = business_json['categories']
+
+            if 'Restaurants' in business_category:
+                restaurant_list.append(business_json['business_id'])
+                
     print(len(restaurant_list))
 
     return restaurant_list
 
 def get_reviews(restaurant_list):
-    review_json_list = get_json('review_100.json')
+    with open(data_dir + 'reviews.txt', 'w') as fw:
 
-    print len(review_json_list)
+        with open(raw_review_file, 'r') as f:
+            while True:
+                line = f.readline()
+                if not line:
+                    break
 
-    restaurant_reviews = []
+                review_json = json.loads(line)            
 
-    for review_json in review_json_list:
-        if review_json['business_id'] in restaurant_list:
-            restaurant_reviews.append(review_json)
-
-    print len(restaurant_reviews)
-
+                if review_json['business_id'] in restaurant_list:
+                    fw.write(review_json['text'])
+                    
+    
 def main():
     restaurant_list = get_restaruants()
 
